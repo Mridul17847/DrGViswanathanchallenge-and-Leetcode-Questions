a4665bd2560a6c123924a91a2921dfbb280af0d1
Mridul Tyagi <mridultyagi49@gmail.com>
2026-08-01 11:23:21 +0530

    Time: 2118 ms (12.85%), Space: 42 MB (63.17%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1;i<=num/2;i++){
+            if(num%i == 0){
+                list.add(i);
+            }
+        }
+        int sum = 0;
+        for(int i =0 ;i<list.size();i++){
+            sum +=list.get(i);
+        }
+        if(sum == num){
+            return true;
+        }
+        return false;
+    }
+}
