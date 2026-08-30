44788832e373a1430e69b0d418e5e7905925c309
Mridul Tyagi <mridultyagi49@gmail.com>
2026-08-30 14:15:04 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.48%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,14 @@
 class Solution {
-    static int[] dp;
-    public int fibo(int n){
-        if(n<2) return n;
-        if(dp[n] != 0) return dp[n];
-        int ans = fibo(n-1) + fibo(n-2);
-        dp[n] = ans;
-        return ans;
-    }
     public int fib(int n) {
-        dp = new int[n+1];
-        return fibo(n);
+        if(n<=1) return n ;
+        int[] dp = new int[3];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            dp[2] = dp[0]+dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
     }
 }
